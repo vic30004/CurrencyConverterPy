9feba993cbb93d216e0aca981c5626c57b974695
vic30004 <victor_993@msn.com>
2020-01-23 13:25:34 +0200

    started setting up the window

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ from tkinter import *
 
 # create initial class
 
-
+class CurrencyConveret:
+    def __init__(self):
+        window = Tk()
+        window.title("Currency Converter")
+        window.configure(bg="#333")
+        
 
 
 # create the application window using Tk()
