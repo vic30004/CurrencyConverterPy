9cdf4ecafc5ddd12d303a3a0c338606079e6594b
vic30004 <victor_993@msn.com>
2020-01-23 13:30:44 +0200

    set up the widget for the app

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 # Import modules
-from tkinter import * 
+from tkinter import *
 
 
 # create initial class
 
 class CurrencyConveret:
     def __init__(self):
+      # create the application window using Tk()
         window = Tk()
         window.title("Currency Converter")
         window.configure(bg="#333")
-        
 
-
-# create the application window using Tk()
-
+  # Add a widget label to the app window
+        Label(window, font="Helvetica 12 bold", bg="#333",
+              text="Amount to Convert").grid(row=1, column=1, sticky=W)
+        Label(window, font="Helvetica 12 bold", bg="#333",
+              text="Conversion Rate").grid(row=2, column=1, sticky=W)
+        Label(window, font="Helvetica 12 bold", bg="#333",
+              text="Conversion Amount").grid(row=3, column=1, sticky=W)
 
 # style the app
